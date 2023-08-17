aec7c6406e20d9f35c654b103bdd8e00912e2425
Dicavimer <diana-vidal-10@hotmail.com>
2023-08-17 09:12:45 -0500

    AddedTexts

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
-  const Lista = [{ Imagen: "https://i.pinimg.com/564x/4a/53/12/4a5312afe9ea2793df25af5d4a456288.jpg", Titulo: "Ten paz", Contenido: "Versículo", Boton: "Detalles",id:"1" },
-  { Imagen: "https://i.pinimg.com/564x/78/fb/2e/78fb2e05b397cb78cd3b2908484f4ee9.jpg", Titulo: "Felicidad", Contenido: "Versículo", Boton: "Detalles",id:"2" },
-  { Imagen: "https://i.pinimg.com/564x/cb/aa/62/cbaa6291eed58c83eaab4096808980d3.jpg", Titulo: "Confía", Contenido: "Versículo", Boton: "Detalles", id:"3" },
-  { Imagen: "https://i.pinimg.com/564x/f1/03/88/f103886c54e6e25279a76375ed5761df.jpg", Titulo: "Honra", Contenido: "Versículo", Boton: "Detalles", id:"4" },
-  { Imagen: "https://i.pinimg.com/564x/b7/50/67/b750671140a6545b818bc5b8246d1998.jpg", Titulo: "La oración", Contenido: "Versículo", Boton: "Detalles", id:"5" },
-  { Imagen: "https://i.pinimg.com/564x/69/91/d6/6991d63bf2901c86774f4091e13d0e3d.jpg", Titulo: "Descansa", Contenido: "Versículo", Boton: "Detalles", id:"6"}]
+  const Lista = [{ Imagen: "https://i.pinimg.com/564x/4a/53/12/4a5312afe9ea2793df25af5d4a456288.jpg", Titulo: "Ten paz", Contenido: "Echando toda vuestra ansiedad sobre Él, porque Él tiene cuidado de vosotros.", Cita: "1 Pedro 5:7", Boton: "Detalles",id:"1" },
+  { Imagen: "https://i.pinimg.com/564x/78/fb/2e/78fb2e05b397cb78cd3b2908484f4ee9.jpg", Titulo: "Felicidad", Contenido: "Regocijaos en el Señor siempre. Otra vez digo: ¡Regocijaos!", Cita: "Filipenses 4:4", Boton: "Detalles",id:"2" },
+  { Imagen: "https://i.pinimg.com/564x/cb/aa/62/cbaa6291eed58c83eaab4096808980d3.jpg", Titulo: "Confía", Contenido: "Encomienda a Jehová tus obras, Y tus pensamientos serán afirmados.", Cita: "Proverbios 16:3", Boton: "Detalles", id:"3" },
+  { Imagen: "https://i.pinimg.com/564x/f1/03/88/f103886c54e6e25279a76375ed5761df.jpg", Titulo: "Honra", Contenido: "Honra a tu padre y a tu madre, para que tus días se alarguen en la tierra que Jehová tu Dios te da.", Cita: " Exodo 20:11", Boton: "Detalles", id:"4" },
+  { Imagen: "https://i.pinimg.com/564x/b7/50/67/b750671140a6545b818bc5b8246d1998.jpg", Titulo: "La oración", Contenido: "Velad y orad, para que no entréis en tentación; el espíritu a la verdad está dispuesto, pero la carne es débil.",Cita: "Mateo 26:41",  Boton: "Detalles", id:"5" },
+  { Imagen: "https://i.pinimg.com/564x/69/91/d6/6991d63bf2901c86774f4091e13d0e3d.jpg", Titulo: "Descansa", Contenido: "Venid a mí todos los que estáis trabajados y cargados, y yo os haré descansar.", Cita: "Mateo 11:28", Boton: "Detalles", id:"6"}]
 
   return (
     <>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,35 +3,30 @@ import React from 'react'
 
 
 const Card = (props) => {
-
     return (
-        <div className='container'>
+        <div className= "container">
             {
                 props.Lista.map((ListaTwo)=>(
                     <div key={ListaTwo.id}>
-                    {/* <div className="contenedorp" >
-                        <div className="columna"> */}
                         <div className="card h-100" >
                             <img src={ListaTwo.Imagen} className="card-img-top" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">{ListaTwo.Titulo}</h5>
-                                <h6>Contador: {props.Count}</h6>
+                                <h6 className="Content">Contador:{props.Count}</h6>
                                 <p className="card-text">{ListaTwo.Contenido}</p>
+                                <p className="card-cita">{ListaTwo.Cita}</p>
                             </div>
                             <div className="footer">
-                                <a href="">{ListaTwo.Boton}</a>
+                                <a href="https://www.biblia.es/reina-valera-1960.php">{ListaTwo.Boton}</a>
                             </div>
                         </div>
                     </div>
-                // </div>
-                // </div>
                 ))
 
                 
             }
 
         </div>
-        // className="row row-cols-1 row-col-md-3 g-4"
     )
 }
 export default Card
