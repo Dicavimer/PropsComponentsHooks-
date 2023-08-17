769932613fe2c3c160ab7391b782b4f01a76a117
Dicavimer <diana-vidal-10@hotmail.com>
2023-08-16 23:56:53 -0500

    DesignV1

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,20 @@ import Contador from './components/Contador'
 
 function App() {
   const [count, setCount] = useState(0)
-  const Lista = [{ Imagen: "./Imagenes/Eagerness.jpg", Titulo: "Ten paz", Contenido: "Versículo", Boton: "Detalles",id:"1" },
-  { Imagen: "./Imagenes/Hapy.jpg", Titulo: "Felicidad", Contenido: "Versículo", Boton: "Detalles",id:"2" },
-  { Imagen: "./Imagenes/Herence.jpg", Titulo: "Hijos", Contenido: "Versículo", Boton: "Detalles", id:"3" },
-  { Imagen: "./Imagenes/Honor.jpg", Titulo: "Honra", Contenido: "Versículo", Boton: "Detalles", id:"4" },
-  { Imagen: "./Imagenes/Pray.jpg", Titulo: "La oración", Contenido: "Versículo", Boton: "Detalles", id:"5" },
-  { Imagen: "./Imagenes/Rest.jpg", Titulo: "Descansa", Contenido: "Versículo", Boton: "Detalles", id:"6"}]
+  const Lista = [{ Imagen: "https://i.pinimg.com/564x/4a/53/12/4a5312afe9ea2793df25af5d4a456288.jpg", Titulo: "Ten paz", Contenido: "Versículo", Boton: "Detalles",id:"1" },
+  { Imagen: "https://i.pinimg.com/564x/78/fb/2e/78fb2e05b397cb78cd3b2908484f4ee9.jpg", Titulo: "Felicidad", Contenido: "Versículo", Boton: "Detalles",id:"2" },
+  { Imagen: "https://i.pinimg.com/564x/cb/aa/62/cbaa6291eed58c83eaab4096808980d3.jpg", Titulo: "Confía", Contenido: "Versículo", Boton: "Detalles", id:"3" },
+  { Imagen: "https://i.pinimg.com/564x/f1/03/88/f103886c54e6e25279a76375ed5761df.jpg", Titulo: "Honra", Contenido: "Versículo", Boton: "Detalles", id:"4" },
+  { Imagen: "https://i.pinimg.com/564x/b7/50/67/b750671140a6545b818bc5b8246d1998.jpg", Titulo: "La oración", Contenido: "Versículo", Boton: "Detalles", id:"5" },
+  { Imagen: "https://i.pinimg.com/564x/69/91/d6/6991d63bf2901c86774f4091e13d0e3d.jpg", Titulo: "Descansa", Contenido: "Versículo", Boton: "Detalles", id:"6"}]
 
   return (
     <>
-      <h2>Practice one</h2>
-      <Card Lista={Lista} Count={count}/>
-      <Contador setCount={setCount} />
+      <div className="contador">
       <h2>Contador: {count}</h2>
+      <Contador setCount={setCount} />
+      </div>
+      <Card Lista={Lista} Count={count}/>
     </>
 
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
+
+
 const Card = (props) => {
 
     return (
-        <div className="container">
+        <div className='container'>
             {
                 props.Lista.map((ListaTwo)=>(
-                    <div className="row row-cols-1 row-cols-md-6 g-4"  key={ListaTwo.id}>
-                    <div className="col">
+                    <div key={ListaTwo.id}>
+                    {/* <div className="contenedorp" >
+                        <div className="columna"> */}
                         <div className="card h-100" >
                             <img src={ListaTwo.Imagen} className="card-img-top" alt="..." />
                             <div className="card-body">
@@ -15,17 +18,20 @@ const Card = (props) => {
                                 <h6>Contador: {props.Count}</h6>
                                 <p className="card-text">{ListaTwo.Contenido}</p>
                             </div>
-                            <div className="card-footer">
+                            <div className="footer">
                                 <a href="">{ListaTwo.Boton}</a>
                             </div>
                         </div>
                     </div>
-                </div>
+                // </div>
+                // </div>
                 ))
+
                 
             }
 
         </div>
+        // className="row row-cols-1 row-col-md-3 g-4"
     )
 }
 export default Card
